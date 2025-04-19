c274e7c60c4dd176eedf994dd586a421c47b856a
Allen Almario <allenalmario@gmail.com>
2025-04-18 21:22:26 -0700

    adds solution to 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
 const reverseString = function() {
-
+    let hello = "hello";
+    let olleh = "";
+    for (let i = hello.length - 1; i >= 0; i--) {
+        olleh+=hello[i]
+    }
+    return olleh;
 };
 
 // Do not edit below this line
