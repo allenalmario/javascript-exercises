e2bb1066cd95f1ea4185bdcbad0b81e86f602ab3
Allen Almario <allenalmario@gmail.com>
2025-04-19 20:59:19 -0700

    adds solution for 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
+    // loop through array
+    for (let i = 0; i < array.length; i++) {
+        if (values.includes(array[i])) {
+            array.splice(i, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
