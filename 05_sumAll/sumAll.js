de83c911c98f0ceff06a46357beab6e2116535e8
Allen Almario <allenalmario@gmail.com>
2025-04-20 19:11:56 -0700

    add solution to 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    // check if either number is negative, if so, break
+    // else
+    // for loop from num1 to num2, init num1, as long as i is <= num2, then end
+    // add the current iterator to var sum
+    // return sum
+    if (num1 < 0 && num2 < 0) {
+        return "ERROR";
+    }
+    else {
+        for (let i = num1; i <= num2; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 
 };
 
