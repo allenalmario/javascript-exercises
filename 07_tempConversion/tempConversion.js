574bb05ffe389007dfe85574d10cf3739562b042
Allen Almario <allenalmario@gmail.com>
2025-04-20 19:50:02 -0700

    adds solution for 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let celcius = (f-32) / 1.8;
+  return celcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let fahrenheit = (c*1.8) + 32;
+  return fahrenheit;
 };
 
 // Do not edit below this line
